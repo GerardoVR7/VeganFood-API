dfab08b927c3a1ff3c3143a0164b323f2d393e0c
GerardoVR7 <GvrZero_07@outlook.com>
2022-10-16 23:34:52 -0500

    arreglo de cors

--- Dockerfile
+++ Dockerfile
@@ -14,4 +14,4 @@ RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 COPY ./service  /code/app
 
 # 
-CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8000"]
+CMD ["uvicorn", "app.main:app", "--host", "0.0.0.0", "--reload", "--port", "8000"]
--- config/database.py
+++ config/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine, MetaData
 
-#engine = create_engine("mysql+pymysql://services_api:3$Q66^2Uqp1N@veganfood.cbxabxrm8scs.us-west-1.rds.amazonaws.com:3306/vegan_food")
 engine = create_engine("mysql+pymysql://admin:G7v3R2001@veganfooddb.ckn3utwh3nqz.us-east-1.rds.amazonaws.com:3307/pruebas")
-
+#engine = create_engine("mysql+pymysql://gerardo:G7v3R2001@localhost:3306/pruebas")
 meta = MetaData()
 
 conn = engine.connect()
